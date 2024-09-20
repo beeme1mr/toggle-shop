4e710871472f0b49a0f3f2195eaed69c5830b314
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-09-20 13:35:43 -0400

    finishing adding product
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <Image
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="/placeholder.svg"
+            src="/img/placeholder.svg"
             width={800}
             height={600}
             alt="Toggles"
