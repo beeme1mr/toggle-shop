9530db44bb0206fe1ec01e833c29dc7bfc234075
Todd Baert <todd.baert@dynatrace.com>
2024-09-20 16:17:46 -0400

    feat: add tracking call
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- src/providers/open-feature.tsx
+++ src/providers/open-feature.tsx
@@ -1,14 +1,28 @@
 "use client";
 
 import {
+  EvaluationContext,
+  Logger,
+  OccurrenceDetails,
   OpenFeatureProvider as OFProvider,
   OpenFeature,
+  Provider,
 } from "@openfeature/react-sdk";
 import { OFREPWebProvider } from "@openfeature/ofrep-web-provider";
 
+class OFREPTrackingWebProvider extends OFREPWebProvider implements Provider {
+  constructor(options: ConstructorParameters<typeof OFREPWebProvider>[0], logger?: Logger) {
+    super(options);
+  }
+
+  track(occurrenceKey: string, context: EvaluationContext, occurrenceDetails: OccurrenceDetails): void {
+      console.log(arguments, undefined, 4);
+  }
+}
+
 // /ofrep/v1/evaluate/flags
 OpenFeature.setProvider(
-  new OFREPWebProvider({
+  new OFREPTrackingWebProvider({
     baseUrl: "http://localhost:3000/api",
   })
 );
