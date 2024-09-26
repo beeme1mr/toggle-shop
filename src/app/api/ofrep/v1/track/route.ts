33f18eaaf243724b4301cd279073519d414a6f7d
Michael Beemer <beeme1mr@users.noreply.github.com>
2024-09-26 08:21:35 -0400

    add new telemetry routes
    Signed-off-by: Michael Beemer <beeme1mr@users.noreply.github.com>

---
+++ src/app/api/ofrep/v1/track/route.ts
@@ -0,0 +1,14 @@
+import { events } from "@opentelemetry/api-events";
+
+const featureFlagTrack = events.getEventLogger("feature_flag");
+
+export async function POST(request: Request) {
+  const attributes = await request.json();
+
+  featureFlagTrack.emit({
+    name: "feature_flag.track",
+    attributes,
+  });
+
+  return new Response(null, { status: 202 });
+}
